bdb617eaeed99af18ef1bff592e22f664e5a5fae
Stephen Rudolph <srudolph@credera.com>
2017-04-06 13:24:57 -0500

    Instrumenting Spring Boot web app

--- spring-web/src/main/java/com/credera/demo/opentracing/web/Application.java
+++ spring-web/src/main/java/com/credera/demo/opentracing/web/Application.java
@@ -1,10 +1,32 @@
 package com.credera.demo.opentracing.web;
 
+import brave.opentracing.BraveTracer;
+import io.opentracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import zipkin.reporter.AsyncReporter;
+import zipkin.reporter.okhttp3.OkHttpSender;
 
 @SpringBootApplication
 public class Application {
+    @Bean
+    Tracer zipkinTracer() {
+        // Configure a reporter, which controls how often spans are sent
+        //   (the dependency is io.zipkin.reporter:zipkin-sender-okhttp3)
+        OkHttpSender sender = OkHttpSender.create("http://127.0.0.1:9411/api/v1/spans");
+        AsyncReporter reporter = AsyncReporter.builder(sender).build();
+
+        // Now, create a Brave tracer with the service name you want to see in Zipkin.
+        //   (the dependency is io.zipkin.brave:brave)
+        brave.Tracer braveTracer = brave.Tracer.newBuilder()
+                .localServiceName("spring-boot-web")
+                .reporter(reporter)
+                .build();
+
+        // Finally, wrap this with the OpenTracing Api
+        return BraveTracer.wrap(braveTracer);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
